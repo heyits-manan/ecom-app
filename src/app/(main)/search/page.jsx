a554dd8f6b938a0fb92c80c6a872a5bb9035b637
Manan Chataut <mananchataut@gmail.com>
2024-10-17 12:32:04 +0530

    fix: Final Search Update

--- src/app/(main)/search/page.jsx
+++ src/app/(main)/search/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Suspense } from "react";
 import ProductSearch from "@/components/ProductSearch";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
